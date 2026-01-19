d99b1e9d0e92e3ada1e2bf02449d3b6000f8e092
Guy Acevedo Anaya <guy.acevedo@comercializadora-neymar.com>
2026-01-19 14:12:57 -0500

    feat: Add a floating WhatsApp button component and integrate it into the main layout.

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,18 +2,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../header/header';
 import { Footer } from "../footer/footer";
+import { FloatingWhatsapp } from '../floating-whatsapp/floating-whatsapp';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, FloatingWhatsapp],
   template: `
     <app-header></app-header>
     <main>
       <router-outlet></router-outlet>
     </main>
     <app-footer></app-footer>
+    <app-floating-whatsapp></app-floating-whatsapp>
   `,
 })
 export class LayoutComponent {}
